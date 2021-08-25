f105409d24a0f6b9e067cf5f70b4bdeccba9ff19
DinaTaklit <ez_taklit@esi.dz>
2021-08-25 06:04:48 +0100

    feat: context: add global state

--- src/context.js
+++ src/context.js
@@ -1,8 +1,18 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 
 const AppContext = React.createContext()
 
 export const AppProvider = ({children}) => {
+
+    // State for typed word
+    const [word, setWord] = useState("")
+    // State for the selected category
+    const [category, setCategory] = useState("en")
+    // State for list of meanings 
+    const [meanings, setMeanings] = useState([])
+    // State to switch between dark/light mode
+    const [darkMode, setDarkMode] = useState(true)
+
     return (
         <AppContext.Provider value={{}}>
             {children}
