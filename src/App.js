5afe71ff6dbadad6fc238e27908d9947812f58dd
DinaTaklit <ez_taklit@esi.dz>
2021-08-25 20:28:13 +0100

    feat:app: implement app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import { Container } from "@material-ui/core";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <main 
+      style={{
+        backgroundColor:'#282c34',
+        color:'white'    
+      }}
+    >
+      <Container
+        maxWidth="md" 
+        id="appContainer"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Header/>
+      </Container>
+    </main>
+
   );
 }
 
