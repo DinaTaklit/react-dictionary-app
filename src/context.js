df5bd356a4e4aaff94b039afeb89cb51bf3154a9
DinaTaklit <ez_taklit@esi.dz>
2021-08-25 05:34:17 +0100

    feat: useContext

---
+++ src/context.js
@@ -0,0 +1,13 @@
+import React, {useContext} from 'react'
+
+const AppContext = React.createContext()
+
+export const AppProvider = ({children}) => {
+    return (
+        <AppContext.Provider value={{}}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => useContext(AppContext)
