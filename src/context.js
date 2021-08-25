4c108aac3421d99eb957e5d471283fd833b41769
DinaTaklit <ez_taklit@esi.dz>
2021-08-25 06:08:26 +0100

    feat: context: add function that fetch the word meanings from the api

--- src/context.js
+++ src/context.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, {useContext, useState, useEffect} from 'react'
 
 const AppContext = React.createContext()
@@ -12,6 +13,16 @@ export const AppProvider = ({children}) => {
     const [meanings, setMeanings] = useState([])
     // State to switch between dark/light mode
     const [darkMode, setDarkMode] = useState(true)
+
+    // Function that fetch meanings from the dictionary
+    const fetchMeanings = async () => {
+        try {
+            const {data} = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`)
+            setMeanings(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <AppContext.Provider value={{}}>
