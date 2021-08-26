4ad9b545c9b169be838c6d8f3aae4bb510cae1d0
DinaTaklit <ez_taklit@esi.dz>
2021-08-26 13:55:13 +0100

    feat:Header: dark/light mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import { MenuItem, TextField } from '@material-ui/core'
+import { MenuItem, TextField, createTheme, ThemeProvider } from '@material-ui/core'
 import React from 'react'
 import { useGlobalContext } from '../../context'
 import countries from '../../category'
@@ -6,12 +6,24 @@ import './Header.css'
 
 function Header() {
     // Get global state
-    const {word, setWord, category, setCategory, setMeanings} = useGlobalContext()
+    const {word, setWord, category, setCategory, setMeanings,darkMode} = useGlobalContext()
 
+    // Functiokn that handle the change of the selected language
     const handleChange = (event) => {
         setCategory(event.target.value)
         setMeanings([])
     }
+
+    // Creatr the theme
+   const darkTheme = createTheme({
+        palette: {
+            primary:{
+                main: darkMode ? '#fff' : '#000'
+            },
+            type: darkMode ? 'dark' : 'light'
+        },
+   })
+
 
     return (
         <div className="header">
@@ -19,30 +31,32 @@ function Header() {
                 {word ? word : 'Word Hunt'}
             </span>
             <div className="inputs">
-                <TextField
-                    className="search"
-                    label="search a Word"
-                    onChange={(e) => setWord(e.target.value)}
-                >
-                </TextField>
-                <TextField
-                    select
-                    label="Language"
-                    value={category}
-                    onChange={(e) => handleChange(e)}
-                    className='select'
-                >
-                    {
-                        countries.map((country) => (
-                            <MenuItem 
-                                key={country.label} 
-                                value={country.label}
-                            >
-                                {country.value}
-                            </MenuItem>
-                        ))
-                    }
-                </TextField>
+                <ThemeProvider theme={darkTheme}>
+                    <TextField
+                        className="search"
+                        label="search a Word"
+                        onChange={(e) => setWord(e.target.value)}
+                    >
+                    </TextField>
+                    <TextField
+                        select
+                        label="Language"
+                        value={category}
+                        onChange={(e) => handleChange(e)}
+                        className='select'
+                    >
+                        {
+                            countries.map((country) => (
+                                <MenuItem 
+                                    key={country.label} 
+                                    value={country.label}
+                                >
+                                    {country.value}
+                                </MenuItem>
+                            ))
+                        }
+                    </TextField>
+                </ThemeProvider>
             </div>
         </div>
     )
