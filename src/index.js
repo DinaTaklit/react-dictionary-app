6dde8f75e600368751efdbcca4d5c807e118d67a
DinaTaklit <ez_taklit@esi.dz>
2021-08-26 15:23:16 +0100

    feat: pwa: add service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import {AppProvider} from './context';
 
 ReactDOM.render(
@@ -14,3 +16,8 @@ ReactDOM.render(
   ,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
